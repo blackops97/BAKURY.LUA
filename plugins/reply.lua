a536aef63bb33fb3e8e72beff138518efb939afc
blackops97 <blackops97@users.noreply.github.com>
2017-01-26 20:10:43 +0000

    Create reply.lua

---
+++ plugins/reply.lua
@@ -0,0 +1,52 @@
+local function run(msg, matches)
+if matches[1] == 'روك'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1, 'ها حياتي عوفني بشغلي 👨‍💻️', 1)
+end
+
+
+if matches[1] == 'ملاك'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1,  'ملكه على عرش قلب مطوري بكر😍🙊', 1)
+end
+
+if matches[1] == 'سفاح'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1,  'تاج راسكم حامي عرضكم  تاج راس مطوري بكر يعني اخوه بكر يعني فديتهم اثنينهم 😍😊', 1)
+end
+
+
+if matches[1] == 'صادق'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1,  'هذا الذيب منو يلحكا😍🙊', 1)
+end
+
+
+if matches[1] == 'المافيا'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1, 'المافيا تاج راسكم😈', 1)
+end
+
+if matches[1] == 'الكساسه'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1, 'ذولي الرائاسه😂😂', 1)
+end
+
+
+if matches[1] == 'هلو'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1, 'هلوات حبيبي نورت ', 1)
+end
+
+if matches[1] == 'بكر'  then
+   tg.sendMessage(msg.chat_id, msg.id_, 1, 'تاج راسك 😎 حامي عرضك 😷 ابن المافيا😟 مطوري العزيز 👮 معرفه @ll_B5, 1)
+end
+
+end
+return {
+  patterns = {
+    "^(روك)$",
+     "^(ملاك)$",
+    "^(بكر)$",
+    "^(سفاح)$",
+    "^(صادق)$",
+    "^(الكساسه)$",
+     "^(المافيا)$",
+     "^(هلو)$",
+  },
+  run = run
+}
+
